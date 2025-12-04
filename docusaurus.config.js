db49fe4be7d7fc893d8c0615ad29135f39fba485
Kioga <ericpost@protonmail.com>
2025-12-03 19:50:11 -0800

    just for the git action

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
 
   // Set the production url of your site here
   url: 'https://eric-post.com',
-  // Set the /<baseUrl>/ pathname under which your site is served :)
+  // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
