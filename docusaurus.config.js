f913e35ef916af4355f86d7c0ecc43a08bf389b3
Kioga <ericpost@protonmail.com>
2025-12-03 18:29:37 -0800

    added refrence to the gh-pges deployment branch. It was not included in the template that the video tutorial handed out.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,14 +23,16 @@ const config = {
   url: 'https://ekioga.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ekioga.github.io/',
+  baseUrl: '/docusaurus-blog/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'eKioga', // Usually your GitHub org/user name.
-  projectName: 'ekioga.github.io', // Usually your repo name.
-
+  projectName: 'docusaurus-blog', // Usually your repo name.
+  deploymentBranch: 'gh-pages",',
+  
   onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
